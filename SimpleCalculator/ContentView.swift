df8dbdf0c934d364ade1e7ff3db02e0c4d01db8f
Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>
2023-04-10 23:18:41 +0300

    Add title to main view
    Also add navigationView and Form. Remove VStack and padding()
    
    Signed-off-by: Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>

--- SimpleCalculator/ContentView.swift
+++ SimpleCalculator/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        NavigationView {
+            Form {
+            }
+            .navigationTitle("Calculator")
         }
-        .padding()
     }
 }
 
