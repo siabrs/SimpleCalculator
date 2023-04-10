cce1e22a780b2005e139f438a95eb5f0dba2c6a7
Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>
2023-04-10 18:24:17 +0300

    Add initial project
    Signed-off-by: Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>

---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Oleksandra Siabrenko on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
